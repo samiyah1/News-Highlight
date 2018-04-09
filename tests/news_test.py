a064ab245f0384028634d9dbf9e1861063b6324b
Mariam <mariam@mariam-Lenovo-ideapad-110-15ISK>
2018-04-09 16:19:54 +0300

    add changes to correct indentation errors

--- tests/news_test.py
+++ tests/news_test.py
@@ -1,9 +1,9 @@
-import unnitest
+import unittest
 from .models import news
 from newsapi import NewsApiClient
 News = news.News
 
-class SourceTest(unnitest.TestCase):
+class SourceTest(unittest.TestCase):
     """
     Test Class to test the behaviour of the News class
     """
@@ -12,7 +12,7 @@ class SourceTest(unnitest.TestCase):
         """
         set up method that will run before every Test
         """
-        self.new_news = newsapi.get_sources()
+         self.new_news = news("abc-news","ABC News","Your trusted source for breaking news, analysis,exclusive interviews,headlines,and videos at ABCNews.com",http://abcnews.go.com,"general","en","us")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
