98b92b4b31a3be84a44b9c649ce37c8c5ba5b6ca
Mariam <mariam@mariam-Lenovo-ideapad-110-15ISK>
2018-04-09 12:46:30 +0300

    add few changes made

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def index():
     message = 'Hello World'
     return render_template('index.html',message = message)
 
-@app.route('/news/<news_id>')
+@app.route('/news/<int:news_id>')
 def new(movie_id):
     """
     View news page function that returns the news details page and its data
